4756b958f1ca1bdfaac8fa35281c7656268b80fa
pcmxgti <16561338+pcmxgti@users.noreply.github.com>
2020-02-20 21:17:14 -0500

    Increase detail on MFA options
    We now examine each MFA factor and try to present the user with a
    reasonable representation of what device we are referring to.
    
    E.g. old output:
    Select your preferred MFA method and press Enter:
    [0]  DUO     web
    [1]  OKTA    push
    [2]  OKTA    call
    [3]  OKTA    sms
    [4]  GOOGLE  token:software:totp
    [5]  FIDO    webauthn
    [6]  FIDO    webauthn
    [7]  OKTA    token:software:totp
    
    New output:
    Select your preferred MFA method and press Enter:
    [0]  DUO     web                  (DUO)
    [1]  OKTA    push                 (My Phone)
    [2]  OKTA    call                 (+1 XXX-XXX-1212)
    [3]  OKTA    sms                  (+1 XXX-XXX-1212)
    [4]  GOOGLE  token:software:totp  (jane.doe@acme.org)
    [5]  FIDO    webauthn             (MacBook Touch ID)
    [6]  FIDO    webauthn             (YubiKey 5c)
    [7]  OKTA    token:software:totp  (jane.doe@acme.org)
    
    blacked
    
    corrected
    
    ver
    
    'test'
    
    test
    
    tests
    
    tests2

--- README.rst
+++ README.rst
@@ -3,11 +3,14 @@
 
 Generate temporary AWS credentials via Okta.
 
-.. image:: https://github.com/dowjones/tokendito/workflows/Test%2C%20Build%2C%20and%20Publish/badge.svg?branch=master
-    :target: https://github.com/dowjones/tokendito/workflows/Test%2C%20Build%2C%20and%20Publish/badge.svg?branch=master
+.. image:: https://github.com/dowjones/tokendito/workflows/Lint%20and%20Test/badge.svg
+    :target: https://github.com/dowjones/tokendito/actions
 
 .. image:: https://img.shields.io/badge/python-2.7%2C%203.5%2C%203.6%2C%203.7%2C%203.8-blueviolet
     :target: https://pypi.org/project/tokendito/
+
+.. image:: https://github.com/dowjones/tokendito/workflows/Woke/badge.svg
+    :target: https://github.com/dowjones/tokendito/actions
 
 .. image:: https://img.shields.io/badge/license-Apache%202.0-ff69b4
     :target: https://github.com/dowjones/tokendito/blob/master/LICENSE.txt
@@ -19,6 +22,9 @@ Generate temporary AWS credentials via Okta.
 |
 
 .. image:: https://raw.githubusercontent.com/dowjones/tokendito/master/docs/tokendito-scaled.gif
+
+
+**WARNING: Python2.7 and Python3.5 support will be dropped in a near time, pin tokendito version to current if you want to keep it working in automation.**
 
 NOTE: Advanced users may shorten the tokendito interaction to a `single command <https://github.com/dowjones/tokendito/tree/master/docs#single-command-usage>`_.
 
--- tokendito/settings.py
+++ tokendito/settings.py
@@ -1,32 +1,35 @@
 # vim: set filetype=python ts=4 sw=4
 # -*- coding: utf-8 -*-
 """This module is responsible for initialisation of global variables."""
-from __future__ import (absolute_import, division,
-                        print_function, unicode_literals)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-from builtins import (ascii, bytes, chr, dict, filter, hex, input,  # noqa: F401
-                      int, list, map, next, object, oct, open, pow, range,
-                      round, str, super, zip)
 from os.path import expanduser
 import sys
 
 from future import standard_library
+
 standard_library.install_aliases()
 
 
-config_dir = expanduser('~') + '/.aws'
-config_file = config_dir + '/okta_auth'
-aws_config_file = config_dir + '/config'
-aws_shared_credentials_file = config_dir + '/credentials'
-aws_output = 'json'
+config_dir = expanduser("~") + "/.aws"
+config_file = config_dir + "/okta_auth"
+aws_config_file = config_dir + "/config"
+aws_shared_credentials_file = config_dir + "/credentials"
+aws_output = "json"
 aws_profile = None
-aws_region = 'us-east-1'
+aws_region = "us-east-1"
 encoding = sys.stdin.encoding
 mfa_method = None
 mfa_response = None
 okta_aws_app_url = None
+okta_status_dict = {
+    "E0000004": "Authentication failed",
+    "E0000047": "API call exceeded rate limit due to too many requests",
+    "PASSWORD_EXPIRED": "Your password is expired",
+    "LOCKED_OUT": "Your account is locked out",
+}
 okta_org = None
-okta_password = ''
-okta_profile = 'default'
-okta_username = ''
+okta_password = ""
+okta_profile = "default"
+okta_username = ""
 role_arn = None
