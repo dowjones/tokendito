1d47123378a5fb3e31375b9bece86e79ba1edea6
Max Fortun <Max.Fortun@wsj.com>
2023-02-28 12:59:54 -0500

    Refactored to set writable ownership

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/ash -e
 
-chown tokendito:tokendito /home/tokendito/.aws/config /home/tokendito/.aws/credentials /home/tokendito/.config/tokendito/tokendito.ini
+files="/home/tokendito/.aws/config /home/tokendito/.aws/credentials /home/tokendito/.config/tokendito/tokendito.ini"
+stat -c "chown %u:%g %n" $files > /tmp/$$.restore
+chown tokendito:tokendito $files
 su tokendito -c "python tokendito/tokendito.py $*"
-
+. /tmp/$$.restore
